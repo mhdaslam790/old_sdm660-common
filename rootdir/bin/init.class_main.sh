5176fe8e8236f5c73f19824ab2ddb8657d1eb536
Joshua Blanchard <joshua.lee.bbg@gmail.com>
2019-12-02 01:32:55 -0500

    sdm660-common: update init scripts from LA.UM.8.2.r1-04300-sdm660.0
    Change-Id: Ie251e79aefb94a971db54802bcba5097302b92a5
    Signed-off-by: Joshua Blanchard <joshua.lee.bbg@gmail.com>

--- rootdir/bin/init.class_main.sh
+++ rootdir/bin/init.class_main.sh
@@ -39,10 +39,9 @@ vendor_rild_status=`getprop init.svc.vendor.ril-daemon`
 case "$baseband" in
     "apq" | "sda" | "qcs" )
     setprop ro.vendor.radio.noril yes
-    if [ -n "$rild_status" ] || [ -n "$vendor_rild_status" ]; then
-      stop ril-daemon
-      stop vendor.ril-daemon
-    fi
+    stop ril-daemon
+    stop vendor.ril-daemon
+    stop vendor.qcrild
 esac
 
 case "$baseband" in
@@ -52,7 +51,7 @@ case "$baseband" in
         modem=`cat /vendor/firmware_mnt/verinfo/ver_info.txt |
                 sed -n 's/^[^:]*modem[^:]*:[[:blank:]]*//p' |
                 sed 's/.*MPSS.\(.*\)/\1/g' | cut -d \. -f 1`
-        # Check if this is AT 3.0 or below. If so, start ril-daemon 
+        # Check if this is AT 3.0 or below. If so, start ril-daemon
         if [ "$modem" = "AT" ]; then
             version=`cat /vendor/firmware_mnt/verinfo/ver_info.txt |
                     sed -n 's/^[^:]*modem[^:]*:[[:blank:]]*//p' |
