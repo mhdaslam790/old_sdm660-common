a80ae227bd873e6cfeda90ce4b174aa989bc5a75
trpsl <46407147+trpsl@users.noreply.github.com>
2019-12-05 15:07:17 +0100

    sdm660: switch to 2.2 version of sound trigger

--- common.mk
+++ common.mk
@@ -42,9 +42,9 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@5.0-impl \
     android.hardware.audio.effect@2.0-service \
-    android.hardware.soundtrigger@2.0-impl \
+    android.hardware.soundtrigger@2.2-impl \
+    android.hardware.soundtrigger@2.2-service \
     android.hardware.bluetooth@1.0 \
-    android.hardware.soundtrigger@2.0-service \
     audiod \
     audio.a2dp.default \
     audio.primary.sdm660 \
